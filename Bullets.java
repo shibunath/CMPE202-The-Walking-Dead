1eeafe23147b21c727daa58faab4a9f05323ecc3
Ian <chao.nie@sjsu.edu>
2016-04-13 14:59:18 -0700

    added super class for bullets, and other kinds that extend it such as Default bullet. Added API for setting bullet speed

--- Bullets.java
+++ Bullets.java
@@ -14,31 +14,46 @@ public class Bullets extends Actor
      */
     
     private boolean setRot = false;
+    private int speed = 15;
     
     public boolean getSetRot()
     {
         return setRot;
     }
     
+    /**
+     * setSetRot - set the rotation of the bullet, mark rotation as set
+     *
+     * @param  newRot   new rotation
+     */
     public void setSetRot(int newRot)
     {
         setRotation(newRot);
         setRot = true;
     }
     
+    /**
+     * getSpeed - get the speed of this bullet, default is 15
+     */
+    public int getSpeed()
+    {
+        return speed;
+    }
+    
+    /**
+     * setSpeed - set the speed of the bullet
+     *
+     * @param  y   new speed
+     */
+    public void setSpeed(int s)
+    {
+        // put your code here
+        speed = s;
+    }
+
+    
     public void act()
     {
-        // get a reference of the world so that to get a ref of the hero
-        MyWorld myWorldRef = (MyWorld)getWorld();
-        Hero heroRef = myWorldRef.getHero();
-        if (getSetRot() != true)
-        {
-            setSetRot(heroRef.getRotation());
-        }
-        move(10);
-        if(isAtEdge())
-        {
-            myWorldRef.removeObject(this);
-        }
+        
     }    
 }
---
+++ Default.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Default here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Default extends Bullets
+{
+    /**
+     * Act - do whatever the Default wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // get a reference of the world so that to get a ref of the hero
+        MyWorld myWorldRef = (MyWorld)getWorld();
+        Hero heroRef = myWorldRef.getHero();
+        if (getSetRot() != true)
+        {
+            setSetRot(heroRef.getRotation());
+        }
+        move(this.getSpeed());
+        if(isAtEdge())
+        {
+            myWorldRef.removeObject(this);
+        }
+    }    
+}
