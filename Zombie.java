77e6eac92c8df9889fefe6281b2ef29d760b7c5a
Nikhil-PC <nikhil.dhamija@24gmail.com>
2016-04-16 15:04:18 -0700

    Added new superclass for Zombie...

--- Zombie.java
+++ Zombie.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Zombie extends Actor
+public class Zombie extends ZombieActor
 {
     /**
      * Act - do whatever the Zombie wants to do. This method is called whenever
