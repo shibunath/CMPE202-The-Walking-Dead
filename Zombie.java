fcc4906eb1ae180944c071f35e73a86f0fd561fd
Nikhil-PC <nikhil.dhamija@24gmail.com>
2016-04-16 14:56:03 -0700

    Added movement to Zombie class...

--- Zombie.java
+++ Zombie.java
@@ -12,8 +12,19 @@ public class Zombie extends Actor
      * Act - do whatever the Zombie wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int dt=0;
     public void act() 
     {
-        // Add your action code here.
+        int dx =0, dy = 0;
+        int rate = 400-(int)Math.abs(dx)-(int)Math.abs(dy);
+        dt += 2-Greenfoot.getRandomNumber(5)-(int)Math.signum(dt);
+        if(dt < -40){
+            dt=-40;
+        }
+        else if(dt > 40){
+            dt=-40;
+        }          
+        turn(dt);
+        move(dt);
     }    
 }
