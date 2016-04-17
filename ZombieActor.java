61c1ba31d37b10fef955eb8b8fbfbc87c0e0fd92
Nikhil-PC <nikhil.dhamija24@gmail.com>
2016-04-16 20:33:27 -0700

    Added getters and setters to ZombieActor...

--- ZombieActor.java
+++ ZombieActor.java
@@ -8,17 +8,35 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class ZombieActor extends Actor
 {
-    protected static final int QVAL = 100;
+    protected static final int ZVAL = 100; //smoothness value for actor
     
-    private int qR;
-    private int qX, qY;
-    private int vX, vY;
+    private int zR;// rotation value for actor
+    private int zX, zY;// actor location coordinates
+    private int vX, vY;// speed quotients for horizontal and vertical axis
     private boolean rotationalBounce;
     
     public void move(){
-        qX += vX;
-        qY += vY;
-        super.setLocation(qX/QVAL, qY/QVAL);
+        zX += vX;
+        zY += vY;
+        super.setLocation(zX/ZVAL, zY/ZVAL);
         rotationalBounce = false;
+    }
+    
+    public int getZX(){  return zX; }
+    public int getZY(){  return zY; }
+    public int getZR(){  return zR; }
+    public int getVX(){  return vX; }
+    public int getVY(){  return vY; }
+    public void setVX(int speed){    vX = speed; }
+    public void setVY(int speed){    vY = speed; }
+    
+    public void setZRotation(int amount){
+        zR = amount;
+        super.setRotation(zR/ZVAL);
+    }
+    public void setZLocation(int x, int y){
+        zX = x;
+        zY = y;
+        super.setLocation(zX/ZVAL, zY/ZVAL);
     }
 }
