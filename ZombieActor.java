af40b01f40c936fca84ce349d6e00b7b8a9bc7e3
Nikhil-PC <nikhil.dhamija24@gmail.com>
2016-04-16 19:05:00 -0700

    Added some functions to ZombieActor...

--- ZombieActor.java
+++ ZombieActor.java
@@ -8,8 +8,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class ZombieActor extends Actor
 {
-    /**
-     * Act - do whatever the ZombieActor wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */      
+    protected static final int QVAL = 100;
+    
+    private int qR;
+    private int qX, qY;
+    private int vX, vY;
+    private boolean rotationalBounce;
+    
+    public void move(){
+        qX += vX;
+        qY += vY;
+        super.setLocation(qX/QVAL, qY/QVAL);
+        rotationalBounce = false;
+    }
 }
