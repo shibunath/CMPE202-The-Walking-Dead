e622ee38a556336ae6bf9a211cbc46d8f0523436
Nikhil-PC <nikhil.dhamija24@gmail.com>
2016-04-21 12:25:42 -0700

    Addingchanges to ZombieActor class...

--- ZombieActor.java
+++ ZombieActor.java
@@ -8,35 +8,73 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class ZombieActor extends Actor
 {
-    protected static final int ZVAL = 100; //smoothness value for actor
+    protected static final int SMOOTH = 200; //divides the rotation and movement ints for smooth movements
     
-    private int zR;// rotation value for actor
-    private int zX, zY;// actor location coordinates
-    private int vX, vY;// speed quotients for horizontal and vertical axis
-    private boolean rotationalBounce;
+    private int zX, zY; // Zombie location coordinates
+    private int vX, vY; // Zombie speeds along the horizontal axis and the vertial axis
+    private int zR; // Zombie rotation
+   
     
-    public void move(){
-        zX += vX;
-        zY += vY;
-        super.setLocation(zX/ZVAL, zY/ZVAL);
-        rotationalBounce = false;
+    public void move()
+    {
+        zX += vX; // adjust x coordinate value according to horizontal speed
+        zY += vY; // adjust y coordinate value according to vertical speed
+        super.setLocation(zX/SMOOTH, zY/SMOOTH); // set current location of actor
+       
+    }    
+    
+    public void setLocation(int x, int y)
+    {
+        super.setLocation(x, y); // set location of actor
+        zX = getX()*SMOOTH; // set Zombie's x coordinate 
+        zY = getY()*SMOOTH; // set Zombie's y coordinate
+    }
+        
+    public void turn(int amount)
+    {
+        zR = (zR+amount); // adjust the zombie rotation
+        super.setRotation(zR/SMOOTH); // set rotation
+    }
+        
+    public void turnTowards(int x, int y)
+    {
+        super.turnTowards(x, y); // turn towards point
+        zR = getRotation()*SMOOTH; // set rotation
+    }
+      
+    public void setRotation(int angle)
+    {
+        super.setRotation(angle); // set rotation
+        zR = getRotation()*SMOOTH; // set smooth rotation
+    }    
+    
+    public void addForce(int amount, int direction)  // adds extra speed to zombie movement as it approaches the hero
+    {
+        vX += Math.cos((double)direction*Math.PI/(180*SMOOTH))*(double)amount; // after adding horizontal speed
+        vY += Math.sin((double)direction*Math.PI/(180*SMOOTH))*(double)amount; // after adding vertical speed
     }
     
-    public int getZX(){  return zX; }
-    public int getZY(){  return zY; }
-    public int getZR(){  return zR; }
-    public int getVX(){  return vX; }
-    public int getVY(){  return vY; }
-    public void setVX(int speed){    vX = speed; }
-    public void setVY(int speed){    vY = speed; }
+    public void move(int amount, int direction)
+    {
+        int holdX = vX, holdY = vY; // save the current speeds
+        vX = 0; vY = 0; // clear the values
+        addForce(amount, direction);
+        move();
+        vX = holdX; vY = holdY; // restore saved speeds
+    }
     
-    public void setZRotation(int amount){
-        zR = amount;
-        super.setRotation(zR/ZVAL);
-    }
-    public void setZLocation(int x, int y){
-        zX = x;
-        zY = y;
-        super.setLocation(zX/ZVAL, zY/ZVAL);
-    }
+    public void move(int amount)
+    {
+        move(amount, zR);
+    //    rotationalBounce = true;
+    }    
+    
+    // getters and setters
+    protected int getzY() { return zY; }        
+    protected int getZR() { return zR; }        
+    protected int getVX() { return vX; }        
+    protected int getVY() { return vY; }        
+    protected void setVX(int speed) { vX = speed; }   
+    protected void setVY(int speed) { vY = speed; }
+ 
 }
