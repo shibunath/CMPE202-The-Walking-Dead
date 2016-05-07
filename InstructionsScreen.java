715d777ac25a50b0c31e124abd48d82bad61f5dd
Kushal <kushalmalani@gmail.com>
2016-05-06 17:05:03 -0700

    Minor Change

--- InstructionsScreen.java
+++ InstructionsScreen.java
@@ -25,7 +25,8 @@ public class InstructionsScreen extends World
         bg.setFont(new Font("SERIF", Font.BOLD, 24));
         bg.setColor(Color.black); 
         bg.drawString("1. The Key to survive this Zombie Apocalypse is to keep collecting \n lives and bullets to kill zombies.\n",50, 100);
-        bg.drawString("2. Since zombies follow you, moving around and killing them will \n make sure you survive long.",50, 160);        
+        bg.drawString("2. Since zombies follow you, moving around and killing them will \n make sure you survive long.",50, 160);
+        bg.drawString("3. Keeping shooting in all directions. This helps in doesn't pile \n up zombies from a single end",50, 220); 
         prepare();
         
     }
