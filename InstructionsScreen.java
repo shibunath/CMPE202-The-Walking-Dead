33cb4c3f85fd48a0dbb112c78ee4389d18852ead
Kushal <kushalmalani@gmail.com>
2016-05-07 11:55:44 -0700

    instructions screen

--- InstructionsScreen.java
+++ InstructionsScreen.java
@@ -14,7 +14,7 @@ public class InstructionsScreen extends World
      * Constructor for objects of class InstructionsScreen.
      * 
      */
-    static GreenfootImage bullets = new GreenfootImage("ammunition-icon.png");
+    gameContents gc = new gameContents();
     public InstructionsScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
@@ -22,18 +22,22 @@ public class InstructionsScreen extends World
         GreenfootImage bg = new GreenfootImage("back.jpg");
         bg.scale(1000,600);
         setBackground(bg);
-        bg.setFont(new Font("SERIF", Font.BOLD, 24));
+        bg.setFont(new Font("SERIF", Font.BOLD, 16));
         bg.setColor(Color.black); 
-        bg.drawString("1. The Key to survive this Zombie Apocalypse is to keep collecting \n lives and bullets to kill zombies.\n",50, 100);
-        bg.drawString("2. Since zombies follow you, moving around and killing them will \n make sure you survive long.",50, 160);
-        bg.drawString("3. Keeping shooting in all directions. This helps in doesn't pile \n up zombies from a single end",50, 220); 
+        bg.drawString("1. The Key to survive this Zombie Apocalypse is to keep collecting bullets to kill zombies.",50, 100);
+        bg.drawString("2. Since zombies follow you, moving around and killing them will make sure you survive long.",50, 130);      
+        bg.drawString("3. Keeping shooting in all directions. This helps in zombies not piling up from a single end.",50, 160);
+        bg.drawString("4. We recommend you to use an external mouse rather than the mousepad for better experience.", 50, 190);
+        bg.drawString("5. Keep Reloading the gun for more bullets.", 50, 220);       
+        bg.drawString("6. Below are the things you would see inside the game.", 50, 250);
+        addObject(gc, 250, 400);
         prepare();
         
     }
     
     public void prepare()
     {
-          ReturnMenu returnmenu = new ReturnMenu();
+          ReturnMenu returnmenu = new ReturnMenu();         
           addObject(returnmenu, 515, 380);
         //returnmenu.setLocation(507, 372);
         //returnmenu.setLocation(503, 371);
---
+++ gameContents.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ReturnMenu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class gameContents extends Actor
+{
+    public gameContents()
+    {
+        GreenfootImage back = new GreenfootImage("gameContents.png");
+        back.scale(350,250);
+        setImage (back);
+    }
+    
+    public void act() 
+    {
+        
+    }    
+}
